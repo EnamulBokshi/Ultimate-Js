0684a8baec2011a4608711596e27e293229197b4
enamulbokshi <enamulbokshi@gmail.com>
2025-04-01 23:51:10 +0600

    add event delegation examples

--- interactive JavaScript/event-based-js/script.js
+++ interactive JavaScript/event-based-js/script.js
@@ -20,3 +20,48 @@ Thse shouts are called events and it's happeing all the time. Our script code ca
 like this:
  if mouse moves do that, if button is clicked do this, if a key is pressed do that.
 */
+// Event bubling
+// Event bubbling is a way of event propagation in the DOM.
+
+// When an event occurs on an element, it first runs the handlers on it, then on its parent, and so on up to the document.
+// This is called event bubbling because the event bubbles up from the target element to the root of the DOM tree.
+// This is the default behavior of events in JavaScript.
+// On triggering an event, it also gets triggered on its parent element, parent's parent element, and so on.
+//
+
+// Consider the following example:
+document.querySelector("#main-h1").addEventListener("click", (e) => {
+    console.log("Main h1 clicked")
+})
+document.querySelector("body").addEventListener("click", (e)=>{
+    console.log("Body clicked")
+
+})
+
+/* 
+
+ Event delegation 
+
+ Event delegation is a technique that allows you to attach a single event listener to a parent element instead of attaching it to each child element.
+ This is useful when you have a large number of child elements and you want to avoid attaching event listeners to each one.
+
+ */
+// In the following example, we have a list of fruits and we want to attach a click event listener to each item.
+
+const fruits = document.querySelector("#fruits");
+const sentence = document.querySelector("#sentence");
+
+fruits.addEventListener("click", (e)=>{
+ let fruit = e.target.textContent;
+ sentence.textContent += fruit + " "
+});
+
+/*
+Note: 
+e.target is the element that triggered the event, not the element that the event listener is attached to.
+e.currentTarget is the element that the event listener is attached to.
+
+In the above example, when we click on a fruit, e.target will be the fruit that was clicked, and e.currentTarget will be the fruits list.
+// So, if we click on "Apple", e.target will be "Apple" (<li>Apple</li> )and e.currentTarget will be the (ul) fruits list.
+*/
+

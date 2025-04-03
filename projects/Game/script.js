6b141321eca5f5dd68714d7710ec15c2dd0d4b64
enamulbokshi <enamulbokshi@gmail.com>
2025-04-03 12:48:08 +0600

    add scoring system and computer AI logic to follow the ball

--- projects/Game/script.js
+++ projects/Game/script.js
@@ -6,11 +6,23 @@ const height = canvas.height;
 
 
 const BALL_SIZE = 5;
-let ballPosition = {x:20, y:20};
+let ballPosition ;
+
+const MAX_COMPUTER_SPEED = 2;
+
 
 // Ball speed
-let xSpeed = 4;
-let ySpeed = 2;
+let xSpeed;
+let ySpeed;
+
+// Ball starting position
+function initBall(){
+    ballPosition = {
+        x:20,y:30
+    };
+    xSpeed = 4;
+    ySpeed = 2;
+}
 
 // Paddles
 const PADDLE_WIDTH = 5;
@@ -19,6 +31,9 @@ const PADDLE_OFFSET = 10;
 
 let leftPaddleTop = 10;
 let rightPaddleTop = 30;
+
+let leftScore = 0;
+let rightScore = 0;
 
 document.addEventListener("mouseover", e =>{
     rightPaddleTop = e.y - canvas.offsetTop;
@@ -44,6 +59,33 @@ ctx.fillRect(
     width - PADDLE_OFFSET - PADDLE_WIDTH, rightPaddleTop, PADDLE_WIDTH, PADDLE_HEIGHT
 )
 
+    // Draw the scores
+    ctx.font = "20px Arial";
+    ctx.textAlign = "left";
+    ctx.fillText(leftScore.toString(),50,50);
+    ctx.textAlign = "right";
+    ctx.fillText(rightScore.toString(), width - 50, 50);
+}
+
+
+// Computer AI logic to follow the ball
+function followBall(){
+    let ball = {
+        top: ballPosition.y,
+        bottom: ballPosition.y + BALL_SIZE,
+    }
+    let paddle = {
+        top: leftPaddleTop,
+        bottom: leftPaddleTop + PADDLE_HEIGHT,
+    }
+    if(ball.top < paddle.top){
+        leftPaddleTop -= MAX_COMPUTER_SPEED;
+
+    }
+    if(ball.bottom > paddle.bottom){
+        leftPaddleTop += MAX_COMPUTER_SPEED
+    }
+
 }
 
 
@@ -61,6 +103,7 @@ function checkPaddleCollision(ball, paddle){
 function update(){
     ballPosition.x += xSpeed;
     ballPosition.y += ySpeed;
+    followBall();
 }
 
 // Bouncing Near the Paddle Ends
@@ -100,8 +143,11 @@ function checkCollision(){
         let distanceFromTop = ball.top - leftPaddle.top;
         let distanceFromBottom = ball.bottom - leftPaddle.bottom;
         adjustAngle(distanceFromTop, distanceFromBottom);
+        console.log(`top: ${distanceFromTop}, bottom: ${distanceFromBottom}`);
+
         xSpeed = Math.abs(xSpeed);
     }
+
     // Check for collision with right paddles
     if(checkPaddleCollision(ball, rightPaddle)){
         let distanceFromTop = ball.top - rightPaddle.top;
@@ -110,10 +156,21 @@ function checkCollision(){
 
         xSpeed = -Math.abs(xSpeed);
     }
+    
+    if(ball.left < 0){
+        rightScore++;
+        initBall();
+    }
+    if(ball.right > width){
+        leftScore++;
+        initBall();
+    }
 
-    if(ball.left<0 || ball.right > width){
-        xSpeed = -xSpeed;
-    };
+
+    // if(ball.left<0 || ball.right > width){
+    //     xSpeed = -xSpeed;
+    // };
+
     if(ball.top<0 || ball.bottom > height){
         ySpeed = -ySpeed;
     };
@@ -125,4 +182,5 @@ function gameLoop(){
     checkCollision()
     setTimeout(gameLoop, 30)
 }
+initBall();
 gameLoop();
